6fd7f4d7ed5576a2d5eaeced676bb944b7e6ea8b
yan38000 <61967417+yan38000@users.noreply.github.com>
2020-06-12 11:09:04 +0200

    MAJ login.php

---
+++ Interfaces-Web/controllers/Login.php
@@ -0,0 +1,30 @@
+<?php
+      if(isset($_POST['formsend'])){
+        
+        extact($_POST);
+
+        if(!empty($password) && !empty($password) && !empty($email)){
+
+          if($password == $cpassword){
+
+            $option = [
+              'cost' => 12,
+          ];
+          $hashpass = password_hash($password, PASSWORD_BCRYPT, $option);
+
+          include 'include/database.php';
+          global $db;
+
+          $q = $db->prepare("INSERT INTRO users(email,password") VALUES(:email,:password)");
+          $q->execute([
+              'email' => $semail,
+              'password' => $hashpass
+              ]);
+          }
+
+      }else{
+        echo "Les champs ne sont pas tous remplis";
+      }
+  }
+
+  ?>
