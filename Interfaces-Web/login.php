367f3caaec8892ad8a923a0e67f655a4bca8296c
yan38000 <61967417+yan38000@users.noreply.github.com>
2020-06-16 11:22:11 +0200

    MAJ login.html en login.php

---
+++ Interfaces-Web/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<?php
+    include('controllers/disgn.php');
+  ?>
+
+<html lang="fr" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>Page de connexion</title>
+      <link rel="stylesheet" href="css/login.css">
+      <link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    </head>
+  <body>
+    <!--Navbar!-->
+
+
+
+
+<form>
+    <div class="container">
+      <div class="row">
+        <div class=" col  col-sm-8 offset-sm-4 col-md-5 offset-md-5 col-lg-5 offset-lg-5">
+
+
+
+
+      <h3 class="title-connexion">Identification</h3>
+        <div class="form-group">
+
+              <label for="exampleInputEmail1">Identifiant</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+
+        <div class="form-group">
+              <label for="exampleInputPassword1">Mot de passe</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">CONNEXION</button>
+
+      </div>
+    </div>
+  </div>
+
+</form>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</body>
+</html>
