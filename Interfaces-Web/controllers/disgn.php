68e3f045d64a2e9413d68317f84191a5559acced
yan38000 <61967417+yan38000@users.noreply.github.com>
2020-06-16 11:39:33 +0200

    Maj disg.php

--- Interfaces-Web/controllers/disgn.php
+++ Interfaces-Web/controllers/disgn.php
@@ -4,11 +4,8 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>Page de connexion</title>
-<<<<<<< HEAD
+
       <link rel="stylesheet" href="../css/login.css">
-=======
-      <link rel="stylesheet" href="Interfaces-Web/css/login.css">
->>>>>>> cbdc074607fa92415823c5058ed1179a1a8459e3
       <link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     </head>
