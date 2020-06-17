fa2332d0d74c08e2ca875be6314f805970ac2c40
yan38000 <61967417+yan38000@users.noreply.github.com>
2020-06-17 14:10:56 +0200

    nouveau fichier .css

--- Interfaces-Web/espace_client.php
+++ Interfaces-Web/espace_client.php
@@ -8,12 +8,14 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>Page de connexion</title>
-      <link rel="stylesheet" href="css/login.css">
+      <link rel="stylesheet" href="css/espace_client.css">
       <link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     </head>
   <body>
 
+
+<!--
 <form method="post" action="controllers/Login.php">
     <div class="container">
       <div class="row">
@@ -40,6 +42,8 @@
   </div>
 
 </form>
+
+!-->
 
 
 
