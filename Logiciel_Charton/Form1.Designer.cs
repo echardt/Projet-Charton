b3168c3ab6e4184c56ccc6f390058b42cceae806
yan38000 <61967417+yan38000@users.noreply.github.com>
2020-06-23 11:00:03 +0200

    Add files via upload

---
+++ Logiciel_Charton/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Logiciel_Charton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CONNEXION";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(60, 74);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(248, 20);
+            this.username.TabIndex = 1;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(59, 105);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(248, 20);
+            this.password.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(359, 570);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Espace Connexion";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+    }
+}
+
---
+++ Logiciel_Charton/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Logiciel_Charton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection Connection = new MySqlConnection("database=charton; server=localhost; user id=root");
+            MySqlCommand table = new MySqlCommand("Select * from agence where username = '"+username.Text+"' and password= '"+password.Text+"'");
+
+
+            try
+            {
+                Cmd.Open();
+                rdv rdv = new rdv();
+                rdv.Show();
+                this.Hide();
+            }
+            catch
+            {
+                MessageBox.Show("pas bon");
+            }
+        }
+    }
+}
